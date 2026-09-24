a3299aaa4c5ec1153f1f6c2280c41942bee85e4f
niximkopf <niximkopf@proton.me>
2026-09-25 00:47:35 +0200

    something

--- modules/config/git.nix
+++ modules/config/git.nix
@@ -5,8 +5,8 @@
     enable = true;
     settings = {
       user = {
-        Name = "niximkopf"; # Replace with your actual name
-        Email = "niximkopf@proton.me"; # Replace with your actual email
+        Name = ""; # Replace with your actual name
+        Email = ""; # Replace with your actual email
       };
       init.defaultBranch = "main";
       pull.rebase = true;
